c797883b908524a34c1c315effed8a23b1522af0
Peter Scully <peter@peterscully.name>
2021-05-22 20:26:38 +1000

    Implement Analog Clock as some rotating squares
    Because:
    - It gets things rotating
    - The squares will be replaced with clock hands

--- lib/analog_clock.dart
+++ lib/analog_clock.dart
@@ -1,6 +1,82 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class AnalogClock extends StatelessWidget {
+class AnalogClock extends StatefulWidget {
   @override
-  Widget build(BuildContext context) => const Text('Analog');
+  _AnalogClockState createState() => _AnalogClockState();
+}
+
+class _AnalogClockState extends State<AnalogClock> {
+  late Timer _timer;
+  late DateTime time;
+
+  @override
+  void initState() {
+    super.initState();
+    time = DateTime.now();
+
+    _timer = Timer.periodic(
+      const Duration(seconds: 1),
+      (_) => setState(() => time = DateTime.now()),
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => AnalogClockFace(timestamp: time);
+}
+
+class AnalogClockFace extends StatelessWidget {
+  final DateTime timestamp;
+
+  const AnalogClockFace({Key? key, required this.timestamp}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final hours = timestamp.hour;
+    final minutes = timestamp.minute;
+    final seconds = timestamp.second;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: AspectRatio(
+        aspectRatio: 1.0,
+        child: Stack(
+          children: [
+            // Hour Hand
+            Transform.rotate(
+              angle: hoursToRadians(hours),
+              child: Container(
+                color: Colors.red,
+              ),
+            ),
+            // Minute Hand
+            Transform.rotate(
+              angle: minutesSecondsToRadians(minutes),
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+            // Second Hand
+            Transform.rotate(
+              angle: minutesSecondsToRadians(seconds),
+              child: Container(
+                color: Colors.green,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  double hoursToRadians(int n) => n * pi / 6.0;
+
+  double minutesSecondsToRadians(int n) => n * pi / 30.0;
 }
