284969ae11be3547700ddf9f2d177096c872ce4a
Peter Scully <peter@peterscully.name>
2021-05-22 16:36:05 +1000

    Update widget test for clock screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,12 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
-import 'package:flutter_clock/analog_clock.dart';
-import 'package:flutter_clock/digital_clock.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:flutter_clock/analog_clock.dart';
+import 'package:flutter_clock/digital_clock.dart';
 import 'package:flutter_clock/main.dart';
 
 void main() {
-  testWidgets('Clock screen smoke test', (WidgetTester tester) async {
+  testWidgets('Clock screen ', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
@@ -22,9 +15,11 @@ void main() {
     expect(find.byType(DigitalClock), findsOneWidget);
     expect(find.text('Analog Clock'), findsNothing);
     expect(find.byType(AnalogClock), findsNothing);
+    expect(find.text('To Analog'), findsOneWidget);
+    expect(find.text('To Digital'), findsNothing);
 
     // Tap the To Analog action and trigger a frame.
-    await tester.tap(find.byType(ActionChip));
+    await tester.tap(find.widgetWithText(ActionChip, 'To Analog'));
     await tester.pump();
 
     // Verify that Analog Clock is shown.
@@ -32,5 +27,19 @@ void main() {
     expect(find.byType(DigitalClock), findsNothing);
     expect(find.text('Analog Clock'), findsOneWidget);
     expect(find.byType(AnalogClock), findsOneWidget);
+    expect(find.text('To Analog'), findsNothing);
+    expect(find.text('To Digital'), findsOneWidget);
+
+    // Tap the To Analog action and trigger a frame.
+    await tester.tap(find.widgetWithText(ActionChip, 'To Digital'));
+    await tester.pump();
+
+    // Verify that Digital Clock is shown.
+    expect(find.text('Digital Clock'), findsOneWidget);
+    expect(find.byType(DigitalClock), findsOneWidget);
+    expect(find.text('Analog Clock'), findsNothing);
+    expect(find.byType(AnalogClock), findsNothing);
+    expect(find.text('To Analog'), findsOneWidget);
+    expect(find.text('To Digital'), findsNothing);
   });
 }
