328c7d4eb9656054165f1ef409948cc28c4dfde9
Peter Scully <peter@peterscully.name>
2021-05-23 23:37:53 +1000

    Implement settting an alarm
    Alarm is saved and restored from shared_preferences.
    
    To Do:
    - Proper flow for clearing the alarm.  Currently displaying the time
      picker and clicking cancel will clear it.
    - Propagate the alarm to the clock face widget via listening to
      shared_preferences (preferred) or via parameter.

--- lib/clock_screen.dart
+++ lib/clock_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 import 'package:flutter_clock/clock_face.dart';
+import 'package:flutter_clock/preferences.dart';
 
 /// Display the clock, and handle switching between analog and digital clocks
 class ClockScreen extends StatefulWidget {
@@ -10,7 +13,16 @@ class ClockScreen extends StatefulWidget {
 }
 
 class _ClockScreenState extends State<ClockScreen> {
+  final _preferences = Preferences();
+
   bool _analog = false;
+  DateTime? _alarm;
+
+  @override
+  void initState() {
+    super.initState();
+    _preferences.addListener(() => setState(() => _alarm = _preferences.alarm));
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -18,12 +30,13 @@ class _ClockScreenState extends State<ClockScreen> {
           title: _analog ? const Text('Analog Clock') : const Text('Digital Clock'),
           actions: [
             ActionChip(
+              label:
+                  _alarm == null ? const Text('No Alarm') : Text(DateFormat.Hms().format(_alarm!)),
+              onPressed: _setAlarm,
+            ),
+            ActionChip(
               label: _analog ? const Text('To Digital') : const Text('To Analog'),
-              onPressed: () {
-                setState(() {
-                  _analog = !_analog;
-                });
-              },
+              onPressed: _toggleAnalog,
             )
           ],
         ),
@@ -34,4 +47,23 @@ class _ClockScreenState extends State<ClockScreen> {
           ),
         ),
       );
+
+  Future<void> _setAlarm() async {
+    final time = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.fromDateTime(_alarm ?? DateTime.now()),
+    );
+    if (time == null) {
+      setState(() {
+        _alarm = null;
+      });
+    } else {
+      setState(() {
+        _alarm = DateTime(1, 1, 1, time.hour, time.minute);
+      });
+    }
+    _preferences.setAlarm(alarm: _alarm);
+  }
+
+  void _toggleAnalog() => setState(() => _analog = !_analog);
 }
